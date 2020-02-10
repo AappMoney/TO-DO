d9b8a143c41447e2a5b05a0c0f708c3fca9c3bac
AappMoney <aji3385@gmail.com>
2020-02-10 20:17:45 +0500

    add functional

--- APP TODO/javascript/app.js
+++ APP TODO/javascript/app.js
@@ -1,26 +1,224 @@
-class Tasks {
-
+class TODO {
     constructor() {
         this.tasks = [];
     }
 
-    showLenght() {
-        const allCard = document.querySelectorAll("#numSub");
-        allCard.forEach(card => {
-            card.innerHTML = ""
-            const targetId = card.parentNode.parentNode.parentNode.id;
-            for (let i = 0; i < this.tasks.length; i++) {
-                if (this.tasks[i].id === targetId) {
-                    card.innerHTML = `(${this.tasks[i].subTask.length})`
+    addAvtiveClass(elem){
+        const active = document.querySelector(".active");
+        if(active) {
+            active.classList.remove("active")
+            elem.classList.add("active")
+        }else {
+            elem.classList.add("active");
+        }
+    }
+
+    showLength() {
+        this.tasks.forEach(elem => {
+            const lengthSpan = document.querySelectorAll("#lengthSub");
+            lengthSpan.forEach(item => {
+                item.innerHTML = `(${elem.subTask.length})`
+            })
+        });
+    }
+
+    renderCard() {
+        const btnshowAll = document.querySelector("#showAllTaskBtn");
+        this.addAvtiveClass(btnshowAll);
+
+        const taskBlock = document.getElementById("task-list_block");
+        taskBlock.innerHTML = ""
+        this.tasks.forEach(elem => {
+        const divBlock = document.createElement("div")
+        divBlock.classList = "card-item";
+        divBlock.dataset.cardId = elem.id;
+
+        const cantentBlock = `
+        <span class="card-title-block">
+            <i class="bg uk-margin-small-right" style="background-color: ${elem.color}"></i>
+                <span class="title">${elem.title} <span id="lengthSub"></span></span>
+        </span> 
+        <span class="close-btn" uk-icon="icon: close;ratio: .8"></span>
+        `
+        divBlock.innerHTML = cantentBlock;
+        taskBlock.appendChild(divBlock);        
+        });
+    }
+
+    selectPopularColor(){
+       const colorItem = document.querySelectorAll(".color-block");
+       colorItem.forEach(colorI => {
+           colorI.addEventListener("click", e => {
+               e.preventDefault();
+               document.querySelector(".active-color").classList.remove("active-color");
+               colorI.classList.add("active-color");
+           });
+       });
+
+       document.forms["show-input"].addEventListener("submit", e => {
+           e.preventDefault();
+           this.dataPush();
+       });
+    }
+
+    dataPush() {
+        const input = document.getElementById("input-title");
+        const activeColor = document.querySelector(".active-color").getAttribute("data-hex");
+        const dataNow = new Date().toISOString();
+
+        this.tasks.push({
+            title: input.value,
+            id: this.generateID(),
+            color: activeColor,
+            subTask: [],
+            createDate: dataNow
+        });
+        input.value = ""
+
+        this.renderCard();
+        this.showallTask();
+        this.showLength();
+        console.log(this.tasks)
+    }
+    removeCard() {
+        const spanBtn = document.querySelectorAll(".close-btn");
+        spanBtn.forEach(btn => {
+            btn.addEventListener("click", e => {
+                e.preventDefault();
+                console.log(btn.parentNode)
+                const cloneTasks = [...this.tasks];
+                this.tasks = cloneTasks.filter(elem => elem.id != btn.parentNode.getAttribute("data-card-id"));
+
+                this.renderCard();
+                this.showallTask();
+                this.showLength();
+            });
+        });
+
+        const cards = document.querySelectorAll(".card-item");
+        cards.forEach(card => {
+            card.addEventListener("click", e => {
+                e.preventDefault();
+                if(e.target.classList == "title" || e.target.id == "lengthSub" || e.target.classList == "bg" || e.target.classList == "card-item" || e.target.classList == "card-title-block") {
+                    this.addAvtiveClass(card);
+                    const elementTS = this.tasks.filter(elem => elem.id === card.getAttribute("data-card-id"));
+                    this.showItem(elementTS[0]);
+                    return;
                 }
-            }
+            });
         })
     }
 
-    addForm(i, secontItemContent) {
+    showItem(elem) {
+        const secontContent = document.getElementById("contentSecont");
+        secontContent.innerHTML = "";
+
+        const aLink = document.createElement('a');
+        aLink.classList = "uk-link-reset tasks-link uk-margin-top";
+        const aLinkContent = `
+        <h2 class="tasks-title" style="color: ${elem.color}">
+        ${elem.title}
+        <span class="uk-icon tasks-icon" uk-icon="icon: pencil"></span>
+        </h2>
+        `
+
+        const subTaskContent = document.createElement("div");
+        subTaskContent.classList = "class", "tasks-item uk-margin-top";
+        subTaskContent.dataset.subId = elem.id;
+
+        aLink.innerHTML = aLinkContent;
+        secontContent.appendChild(aLink);
+        secontContent.appendChild(subTaskContent);
+
+        if(elem.subTask.length > 0) {
+            this.addSubTask(elem);
+        }
+
+        this.addForm(elem, secontContent);
+        
+
+        document.forms["all-task-form"].addEventListener("submit", e => {
+            e.preventDefault();
+            this.showallTask();
+        });
+    }
+
+    showallTask(){
+        const btnshowAll = document.querySelector("#showAllTaskBtn");
+        this.addAvtiveClass(btnshowAll);
+        const secontContent = document.getElementById("contentSecont");
+        secontContent.innerHTML = "";
+        this.addTitle();
+
+        this.removeCard();
+        // this.showItem();
+    }
+
+    addTitle() {
+        const secontContent = document.getElementById("contentSecont");
+
+        this.tasks.forEach(elem => {
+        const aLink = document.createElement('a');
+        aLink.classList = "uk-link-reset tasks-link uk-margin-top";
+        const aLinkContent = `
+        <h2 class="tasks-title" style="color: ${elem.color}">
+        ${elem.title}
+        <span class="uk-icon tasks-icon" uk-icon="icon: pencil"></span>
+        </h2>
+        `
+
+        const subTaskContent = document.createElement("div");
+        subTaskContent.classList = "tasks-item uk-margin-top";
+        subTaskContent.dataset.subId = elem.id;
+
+        aLink.innerHTML = aLinkContent;
+        secontContent.appendChild(aLink);
+        secontContent.appendChild(subTaskContent);
+
+        if(elem.subTask.length > 0) {
+            this.addSubTask(elem);
+        }
+
+        this.addForm(elem,secontContent);
+        });
+    }
+
+    addSubTask(elem) {
+        const subTaskBlock = document.querySelector(`${elem.id}`);
+        subTaskBlock.innerHTML = ""
+        elem.subTask.forEach(sub => {
+            for(let i = 0 ; i < sub.length; i++) {
+                const taskItem = document.createElement("div")
+                taskItem.classList = "tasks-block uk-margin-bottom";
+    
+                const taskSubTasks = `
+                <div class="checkbox">
+                <input class="task-input" id="tasks-input" type="checkbox">
+                <label class="label-check" for="tasks-input">
+                <span class="label-check-icon" uk-icon="icon: check; ratio: 1"></span>
+                </label>
+                </div>
+                <p class="tasks-text uk-margin-small-left">${sub[i].title}</p>
+                <div class="uk-flex uk-flex-row tasks-item_row">
+                <div>
+                <span class="tasks-edit_icon" uk-icon="icon: pencil; ratio: .8"></span>
+                </div>
+                <div>
+                <span class="tasks-deleted_icon" uk-icon="icon: trash; ratio: .8"></span>
+                </div>
+                </div>
+            `
+    
+                taskItem.innerHTML = taskSubTasks;
+                subTaskBlock.appendChild(taskItem);
+            }
+        });
+    }
+
+    addForm(elem,secontContent) {
         const formBlock = document.createElement("div")
-        formBlock.setAttribute("class", "tasks-form_new uk-flex uk-flex-row uk-flex-middle uk-margin-top");
-        formBlock.setAttribute("id", this.tasks[i].id)
+        formBlock.classList = "tasks-form_new uk-flex uk-flex-row uk-flex-middle uk-margin-top";
+        formBlock.dataset.forId = elem.id;
 
         const formTextContent = `
             <span id="btnshow" class="tasks-form_show">
@@ -29,241 +227,17 @@ class Tasks {
             </span>
             `
         formBlock.innerHTML = formTextContent;
-        secontItemContent.appendChild(formBlock);
+        secontContent.appendChild(formBlock);
+        return;
     }
-
-    addSubTask(i, elem) {
-        elem.innerHTML = ""
-        for (let j = 0; j < this.tasks[i].subTask.length; j++) {
-            const taskItem = document.createElement("div")
-            taskItem.setAttribute("class", "tasks-block uk-margin-bottom")
-
-            const taskSubTasks = `
-            <div class="checkbox">
-            <input class="task-input" id="tasks-input" type="checkbox">
-            <label class="label-check" for="tasks-input">
-            <span class="label-check-icon" uk-icon="icon: check; ratio: 1"></span>
-            </label>
-            </div>
-            <p class="tasks-text uk-margin-small-left">${this.tasks[i].subTask[j].title}</p>
-            <div class="uk-flex uk-flex-row tasks-item_row">
-            <div>
-            <span class="tasks-edit_icon" uk-icon="icon: pencil; ratio: .8"></span>
-            </div>
-            <div>
-            <span class="tasks-deleted_icon" uk-icon="icon: trash; ratio: .8"></span>
-            </div>
-            </div>
-        `
-
-            taskItem.innerHTML = taskSubTasks;
-            elem.appendChild(taskItem);
-        }
-    }
-
-    showALink(linkbtn) {
-        console.log(linkbtn)
-    }
-
-    addTitle(i, secontItemContent) {
-        const aLink = document.createElement('a');
-        aLink.setAttribute("class", "uk-link-reset tasks-link uk-margin-top");
-        const aLinkContent = `
-        <h2 class="tasks-title" style="color: ${this.tasks[i].color}">
-        ${this.tasks[i].title}
-        <span class="uk-icon tasks-icon" uk-icon="icon: pencil"></span>
-        </h2>
-        `
-        aLink.innerHTML = aLinkContent;
-        secontItemContent.appendChild(aLink);
-        const subTaskContent = document.createElement("div");
-        subTaskContent.setAttribute("class", "tasks-item uk-margin-top");
-        subTaskContent.setAttribute("id", this.tasks[i].id);
-        secontItemContent.appendChild(subTaskContent);
-
-        const allLink = document.querySelectorAll(".tasks-title")
-        allLink.forEach(linkbtn => {
-            linkbtn.addEventListener(`click`, () => {
-                this.showALink(linkbtn);
-            })
-        });
-    }
-
-
-
-    showItem() {
-        const cardItemall = document.querySelectorAll(".card-item");
-        cardItemall.forEach(elem => {
-            elem.addEventListener("click", () => {
-                document.querySelector(".active").classList.remove("active");
-                elem.classList.add("active");
-
-                const cardId = elem.id;
-
-                for (let i = 0; i < this.tasks.length; i++) {
-                    if (this.tasks[i].id === cardId) {
-                        const secontItemContent = document.getElementById("contentSecont");
-                        secontItemContent.innerHTML = ""
-
-                        this.addTitle(i, secontItemContent);
-
-                        if (this.tasks[i].subTask.length > 0) {
-                            const elem = document.querySelector(".tasks-item ")
-                            this.addSubTask(i, elem)
-                        }
-
-                        this.addForm(i, secontItemContent);
-                    }
-                }
-                // return;
-                // this.showForm();
-            });
-        });
-
-        document.forms["all-task-form"].addEventListener("submit", e => {
-            e.preventDefault()
-            this.showAllTasks();
-        });
-
-    }
-
-
-    showAllTasks() {
-        const secontItemContent = document.getElementById("contentSecont");
-        secontItemContent.innerHTML = ""
-        document.querySelector(".active").classList.remove("active");
-        const showAllBtn = document.querySelector("#showAllTaskBtn");
-        showAllBtn.classList.add("active")
-
-        for (let i = 0; i < this.tasks.length; i++) {
-            this.addTitle(i, secontItemContent)
-
-            if (this.tasks[i].subTask.length > 0) {
-                const taskBlock = secontItemContent.querySelector(`#${this.tasks[i].id}`)
-                this.addSubTask(i, taskBlock);
-            }
-            this.addForm(i, secontItemContent);
-        }
-        // this.showForm();
-    }
-
 
     generateID() {
-        return Math.random().toString(36).substr(2, 9);
+        return Math.random().toString(36).substr(2,9);
     }
-
-    selectPopularColor() {
-        const selectColorItem = document.querySelectorAll(".color-block");
-        selectColorItem.forEach(elem => {
-            elem.addEventListener("click", e => {
-                e.preventDefault()
-                document.querySelector(".active-color").classList.remove("active-color");
-                elem.classList.add("active-color");
-            });
-        });
-
-        document.forms["show-input"].addEventListener("submit", e => {
-            e.preventDefault();
-            this.dataPush();
-        });
-
-    }
-    deleteTask = (id) => {
-        
-        const preTasks = [...this.tasks];
-        this.tasks = preTasks.filter((item) => item.id !== id);
-        console.log(document.getElementById("showAllTaskBtn"));
-        this.showLenght();
-        this.showAllTasks();
-        this.renderTaskList();
-    }
-
-    renderTaskList = () => {
-        const tasks = this.tasks;
-        const taskList = document.getElementById("task-list_block");
-        taskList.innerHTML = '';
-        tasks.forEach((task, index) => {
-
-            const cardItem = document.createElement("div");
-            cardItem.setAttribute("class", "card-item");
-            cardItem.setAttribute("id", task.id);
-            cardItem.dataset.cardId = task.id;
-
-            const contentList = `
-                <span class="card-title-block" >
-                <i id="${task.color}" class="bg uk-margin-small-right"></i>
-                <span class="title">${task.title} <span id="numSub">()</span></span>
-                </span> 
-            
-            `;
-
-            const closeBtn = document.createElement("span");
-            closeBtn.classList = 'close-btn';
-            closeBtn.setAttribute("uk-icon", "icon: close;ratio: .8");
-            closeBtn.addEventListener("click", () => this.deleteTask(task.id));
-
-            cardItem.innerHTML = contentList;
-            cardItem.appendChild(closeBtn);
-            taskList.appendChild(cardItem);
-        });
-    }
-
-    dataPush() {
-        const formInput = document.getElementById("input-title");
-        const activeColor = document.querySelector(".active-color");
-        const hex = activeColor.getAttribute("data-hex");
-        const date = new Date().toISOString();
-
-        this.tasks.push({
-            title: formInput.value,
-            id: this.generateID(),
-            color: hex,
-            subTask: [],
-            createDate: date
-        });
-        formInput.value = ""
-
-        const lastElemTasks = this.tasks[this.tasks.length - 1];
-
-        const taskList = document.getElementById("task-list_block");
-        const cardItem = document.createElement("div");
-        cardItem.setAttribute("class", "card-item");
-        cardItem.setAttribute("id", lastElemTasks.id);
-
-        cardItem.dataset.cardId = lastElemTasks.id; // unique id for cardItem data-card-id=""
-        // cardItem.dataset["card-id"] = lastElemTasks.id; // unique id for cardItem data-card-id=""
-
-
-        const contentList = `
-        <span class="card-title-block" >
-        <i id="${activeColor.id}" class="bg uk-margin-small-right"></i>
-        <span class="title">${lastElemTasks.title} <span id="numSub">()</span></span>
-        </span> 
-        
-        `
-        // <span class="close-btn" uk-icon="icon: close;ratio: .8" onclick="${this.deleteTask.bind(this, lastElemTasks.id)}" ></span>
-        const closeBtn = document.createElement("span");
-        closeBtn.classList = 'close-btn';
-        closeBtn.setAttribute("uk-icon", "icon: close;ratio: .8");
-        closeBtn.addEventListener("click", () => this.deleteTask(lastElemTasks.id));
-
-
-        cardItem.innerHTML = contentList;
-        cardItem.appendChild(closeBtn);
-        taskList.appendChild(cardItem);
-        this.showLenght()
-        this.showAllTasks();
-        this.showItem();
-
-        document.querySelector('.uk-drop-close').click(); // TODO: please refactor this
-
-        console.log(this.tasks);
-
-    }
-
 }
 
-document.addEventListener("DOMContentLoaded", e => {
-    const tasks = new Tasks();
-    tasks.selectPopularColor();
+
+window.addEventListener("DOMContentLoaded", () => {
+    const todo = new TODO();
+    todo.selectPopularColor();
 });
